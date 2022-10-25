6b4ae42ff6f2d0b57754f3e15a11c2c31f31c91b
rasenshuriken <omerozoglu.1@gmail.com>
2022-10-25 15:33:33 +0300

    feat: Create index.js.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ path: "./config/.env" });
+const http = require("http");
+
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
